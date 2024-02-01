d10efa54ad0cc4a256d4c9e702d7aac36d57f255
duc <duc@appiphany.com.au>
2024-02-01 12:28:42 +0700

    Update gradle

--- example/android/app/src/main/kotlin/de/appgewaltig/disk_space_example/EmbeddingV1Activity.kt
+++ example/android/app/src/main/kotlin/de/appgewaltig/disk_space_example/EmbeddingV1Activity.kt
@@ -2,12 +2,12 @@ package de.appgewaltig.disk_space_example
 
 import android.os.Bundle
 import de.appgewaltig.disk_space.DiskSpacePlugin
-import io.flutter.app.FlutterActivity
-
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
 
 class EmbeddingV1Activity : FlutterActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        DiskSpacePlugin.registerWith(registrarFor("de.appgewaltig.disk_space.DiskSpacePlugin"))
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        flutterEngine.plugins.add(DiskSpacePlugin())
     }
 }
